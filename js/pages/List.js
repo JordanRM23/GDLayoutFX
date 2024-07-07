9a243e23e03e5235e69e4911891c2d2033c38d97
JordanRM23 <157156538+JordanRM23@users.noreply.github.com>
2024-07-07 13:06:40 -0500

    Actualizar List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -86,7 +86,7 @@ export default {
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
                     <div class="og">
-                        <p class="type-label-md">Website inspired by <a href="https://layoutfxlist.000webhostapp.com/main_list.html" target="_blank">LayoutFX List</a></p>
+                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
